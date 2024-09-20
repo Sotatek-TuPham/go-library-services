a60db0969b6dfb568c116223a461a2e25d3fff07
Sotatek-TuPham <129022544+Sotatek-TuPham@users.noreply.github.com>
2024-09-20 10:01:05 +0700

    init src base

---
+++ library-server/DB/db.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	db, err := gorm.Open(postgres.Open(os.Getenv("DB_URL")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate()
+	DB = db
+}
